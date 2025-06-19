58ecb032af6fd976cb13d86b2749b2cc1437eb46
Elanis <elanis@hotmail.com>
2025-06-19 21:07:41 +0200

    Set http.route to "unknown" if we don't have it

--- OpenTelemetryExtensions.cs
+++ OpenTelemetryExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using OpenTelemetry;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
@@ -7,37 +8,44 @@ using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
 namespace Dysnomia.Common.OpenTelemetry {
-	public static class OpenTelemetryExtensions {
-		public static void EnableOpenTelemetry(this IServiceCollection serviceCollection, IHostEnvironment environment) {
-			var openTelemetryBuilder = serviceCollection.AddOpenTelemetry()
-				.WithTracing(otBuilder => {
+    public static class OpenTelemetryExtensions {
+        public static void EnableOpenTelemetry(this IServiceCollection serviceCollection, IHostEnvironment environment) {
+            var openTelemetryBuilder = serviceCollection.AddOpenTelemetry()
+                .WithTracing(otBuilder => {
 
-					otBuilder.AddAspNetCoreInstrumentation()
-					.AddEntityFrameworkCoreInstrumentation()
-					.AddHttpClientInstrumentation()
-					.AddSqlClientInstrumentation()
-					.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(environment.ApplicationName));
-				})
-				.WithMetrics(otBuilder => {
-					otBuilder.AddAspNetCoreInstrumentation()
-					.AddHttpClientInstrumentation()
-					.AddProcessInstrumentation()
-					.AddRuntimeInstrumentation()
-					.AddSqlClientInstrumentation()
-					.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(environment.ApplicationName));
-				})
-				.WithLogging(otBuilder => {
-					otBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(environment.ApplicationName));
+                    otBuilder.AddAspNetCoreInstrumentation(o => {
+                        o.EnrichWithHttpResponse = (activity, response) => {
+                            if (string.IsNullOrWhiteSpace(activity.Tags.FirstOrDefault(x => x.Key == "http.route").Value)) {
+                                activity.DisplayName = "unknown";
+                                activity.SetTag("http.route", "unknown");
+                            }
+                        };
+                    })
+                    .AddEntityFrameworkCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddSqlClientInstrumentation()
+                    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(environment.ApplicationName));
+                })
+                .WithMetrics(otBuilder => {
+                    otBuilder.AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddProcessInstrumentation()
+                    .AddRuntimeInstrumentation()
+                    .AddSqlClientInstrumentation()
+                    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(environment.ApplicationName));
+                })
+                .WithLogging(otBuilder => {
+                    otBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(environment.ApplicationName));
 
-					if (environment.EnvironmentName == "Development") {
-						otBuilder.AddConsoleExporter();
-					}
-				});
+                    if (environment.EnvironmentName == "Development") {
+                        otBuilder.AddConsoleExporter();
+                    }
+                });
 
-			if (environment.EnvironmentName != "Development") {
-				openTelemetryBuilder.UseOtlpExporter();
-			}
-		}
+            if (environment.EnvironmentName != "Development") {
+                openTelemetryBuilder.UseOtlpExporter();
+            }
+        }
 
-	}
+    }
 }
