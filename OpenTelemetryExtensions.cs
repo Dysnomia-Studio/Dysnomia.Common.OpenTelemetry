8d2d1b1b20ad45b5708241e2087414e3cfbdc565
Elanis <elanis@hotmail.com>
2025-06-19 20:06:07 +0200

    Use native metrics instead of extensions

--- OpenTelemetryExtensions.cs
+++ OpenTelemetryExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using OpenTelemetry;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
@@ -7,37 +8,46 @@ using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
 namespace Dysnomia.Common.OpenTelemetry {
-	public static class OpenTelemetryExtensions {
-		public static void EnableOpenTelemetry(this IServiceCollection serviceCollection, IHostEnvironment environment) {
-			var openTelemetryBuilder = serviceCollection.AddOpenTelemetry()
-				.WithTracing(otBuilder => {
+    public static class OpenTelemetryExtensions {
+        public static void EnableOpenTelemetry(this IServiceCollection serviceCollection, IHostEnvironment environment) {
+            var openTelemetryBuilder = serviceCollection.AddOpenTelemetry()
+                .WithTracing(otBuilder => {
 
-					otBuilder.AddAspNetCoreInstrumentation()
-					.AddEntityFrameworkCoreInstrumentation()
-					.AddHttpClientInstrumentation()
-					.AddSqlClientInstrumentation()
-					.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(environment.ApplicationName));
-				})
-				.WithMetrics(otBuilder => {
-					otBuilder.AddAspNetCoreInstrumentation()
-					.AddHttpClientInstrumentation()
-					.AddProcessInstrumentation()
-					.AddRuntimeInstrumentation()
-					.AddSqlClientInstrumentation()
-					.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(environment.ApplicationName));
-				})
-				.WithLogging(otBuilder => {
-					otBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(environment.ApplicationName));
+                    otBuilder
+                    .AddEntityFrameworkCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddSqlClientInstrumentation()
+                    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(environment.ApplicationName));
+                })
+                .WithMetrics(otBuilder => {
 
-					if (environment.EnvironmentName == "Development") {
-						otBuilder.AddConsoleExporter();
-					}
-				});
+                    otBuilder.AddMeter(
+                        "Microsoft.AspNetCore.Hosting",
+                        "Microsoft.AspNetCore.Routing",
+                        "Microsoft.AspNetCore.Diagnostics",
+                        "Microsoft.AspNetCore.RateLimiting",
+                        "Microsoft.AspNetCore.HeaderParsing",
+                        "Microsoft.AspNetCore.Server.Kestrel",
+                        "Microsoft.AspNetCore.Http.Connections"
+                    )
+                    .AddHttpClientInstrumentation()
+                    .AddProcessInstrumentation()
+                    .AddRuntimeInstrumentation()
+                    .AddSqlClientInstrumentation()
+                    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(environment.ApplicationName));
+                })
+                .WithLogging(otBuilder => {
+                    otBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(environment.ApplicationName));
 
-			if (environment.EnvironmentName != "Development") {
-				openTelemetryBuilder.UseOtlpExporter();
-			}
-		}
+                    if (environment.EnvironmentName == "Development") {
+                        otBuilder.AddConsoleExporter();
+                    }
+                });
 
-	}
+            if (environment.EnvironmentName != "Development") {
+                openTelemetryBuilder.UseOtlpExporter();
+            }
+        }
+
+    }
 }
